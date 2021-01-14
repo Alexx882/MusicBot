2b704324d60ff44548c39aa0cd25ad38b52fa335
Herry <herrytco@gmail.com>
2021-01-14 17:00:11 +0100

    extraction of interfaces

---
+++ src/main/java/com/jagrosh/jmusicbot/audio/AudioManager.java
@@ -0,0 +1,36 @@
+package com.jagrosh.jmusicbot.audio;
+
+public interface AudioManager {
+
+    /**
+     * add the track to the end of the queue. play it immediatly if no
+     * tracks are in the queue
+     *
+     * @param track track to enqueue
+     * @return position the track got inserted in the queue
+     */
+    int addTrack(QueuedTrack track);
+
+    /**
+     * add the track to the front of the queue. play it immediatly if no
+     * tracks are in the queue
+     *
+     * @param track track to enqueue
+     * @return 0 if there is a track playing, -1 if immediatly played
+     */
+    int addTrackToFront(QueuedTrack track);
+
+    /**
+     * play the first track from the default playlist
+     *
+     * @return if a track has been found to play
+     */
+    boolean playFromDefault();
+
+    /**
+     * stops the current track end empties the queue
+     */
+    void stopAndClear();
+
+
+}
---
+++ src/main/java/com/jagrosh/jmusicbot/audio/StatusMessageManager.java
@@ -0,0 +1,51 @@
+package com.jagrosh.jmusicbot.audio;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+
+/**
+ * manages messages in the text-channel which displays the current bot-status
+ */
+public interface StatusMessageManager {
+
+    /**
+     * update reference to the given message.
+     *
+     * @param statusMessage contains guild and text-channel information needed
+     */
+    void setLastNPMessage(Message statusMessage);
+
+    /**
+     * clear the message information for the given guild
+     *
+     * @param guild target guild which information should be cleared
+     */
+    void clearLastNPMessage(Guild guild);
+
+    /**
+     * updates the topic of a channel
+     *
+     * @param guildId discord server ID
+     * @param handler audio handler which manages tracks, playlists ...
+     * @param wait    TODO ?
+     */
+    void updateTopic(long guildId, AudioHandler handler, boolean wait);
+
+    /**
+     * event handler triggered when a new track is played or the current one is stopped
+     *
+     * @param guildId discord server ID
+     * @param track   new track which starts playing, is null when the track is stopped
+     * @param handler audio handler which manages tracks, playlists ...
+     */
+    void onTrackUpdate(long guildId, AudioTrack track, AudioHandler handler);
+
+    /**
+     * event handler triggered when a message is deleted from a text-channel in discord
+     *
+     * @param guild     server where the question was deleted
+     * @param messageId id of the deleted message
+     */
+    void onMessageDelete(Guild guild, long messageId);
+}
